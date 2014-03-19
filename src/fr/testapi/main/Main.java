65e49a386f82ccc36f3ac245e97acbc1c3016151
3751Creator <alexandre-schweig@live.fr>
2014-03-19 20:56:58 +0100

    1st

---
+++ src/fr/testapi/main/Main.java
@@ -0,0 +1,33 @@
+package fr.testapi.main;
+
+
+import javax.swing.*;
+
+
+/**
+ * User: Alexandre
+ */
+public class Main {
+
+    public static String Version(){
+        return "4.0";
+    }
+
+    public static void main(String[] args){
+        try{
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        }catch (Exception e){
+            System.out.println("Unable to load Windows look and feel");
+        }
+
+	    Start();
+    }
+    public static void Start(){
+        Frame gui = new Frame();
+        gui.setSize(450, 275);
+        gui.setLocationRelativeTo(null);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.setResizable(false);
+    }
+}
